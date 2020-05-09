21353650b3ade9dcba0de838ba9abdff4349dfc0
Norbert Fejer <norbertfejer@protonmail.com>
2020-05-09 15:32:00 +0300

    Saved login

---
+++ TMCatalog.View/UserControls/Client.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TMCatalog.ViewModel;
+using TMCatalog.ViewModel.UserControls;
+
+namespace TMCatalog.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Client.xaml
+    /// </summary>
+    public partial class Client : UserControl
+    {
+        ClientVM clientVM;
+
+        public Client()
+        {
+            InitializeComponent();
+            clientVM = MainWindowViewModel.Instance.ClientVM;
+        }
+
+        private void RemovePlaceholderText(object sender, RoutedEventArgs e)
+        {
+            if (clientVM.SearchText.Equals(clientVM.PlaceholderText))
+            {
+                clientVM.SearchText = "";
+                clientVM.SearchClient();
+            }
+        }
+
+        private void SetPlaceholderText(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(clientVM.SearchText.Trim()))
+            {
+                clientVM.SearchText = clientVM.PlaceholderText;
+                clientVM.SearchClient();
+            }
+        }
+
+        private void Search(object sender, TextChangedEventArgs e)
+        {
+            clientVM.SearchText = ((TextBox)sender).Text;
+            clientVM.SearchClient();
+        }
+    }
+}
---
+++ TMCatalog.ViewModel/UserControls/ClientVM.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMCatalog.Common.MVVM;
+using TMCatalog.Logic;
+using TMCatalog.Model;
+
+namespace TMCatalog.ViewModel.UserControls
+{
+    public class ClientVM : ViewModelBase
+    {
+        private string placeholderText = "Search by name, card number or phone number...";
+        private List<Client> clientList;
+        private Client selectedClient;
+        private string searchText;
+
+        public ClientVM()
+        {
+            this.SearchText = this.PlaceholderText;
+        }
+
+        public string PlaceholderText
+        {
+            get
+            {
+                return this.placeholderText;
+            }
+        }
+
+        public List<Client> ClientList
+        {
+            get
+            {
+                return this.clientList;
+            }
+
+            set
+            {
+                this.clientList = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public Client SelectedClient
+        {
+            get
+            {
+                return this.selectedClient;
+            }
+
+            set
+            {
+                this.selectedClient = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public string SearchText
+        {
+            get
+            {
+                return this.searchText;
+            }
+
+            set
+            {
+                this.searchText = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public void SearchClient()
+        {
+            if (String.IsNullOrEmpty(SearchText.Trim()) || SearchText.Equals(PlaceholderText))
+            {
+                this.ClientList = Data.Catalog.GetAllClients();
+            }
+            else
+            {
+                if (SearchText.StartsWith("0"))
+                {
+                    this.ClientList = Data.Catalog.SearchClientByPhoneNumber(SearchText);
+                }
+                else
+                {
+                    if (int.TryParse(SearchText, out int cardNumber))
+                    {
+                        this.ClientList = Data.Catalog.SearchClientByCardNumber(cardNumber);
+                    }
+                    else
+                    {
+                        this.ClientList = Data.Catalog.SearchClientByName(SearchText.Trim());
+                    }
+                }
+            }
+        }
+    }
+}
